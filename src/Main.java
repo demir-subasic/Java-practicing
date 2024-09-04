fcd6ef4f8038264d68a8719e8d3b6a78a1d75965
demir-subasic <subasicd2000@gmail.com>
2024-09-04 21:44:57 +0200

    java basics

---
+++ src/Main.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner s=new Scanner(System.in);
+
+        //Rad sa tekstom
+//        String name;
+//        System.out.println("May i know your name");
+//        name = s.nextLine();
+//        System.out.println("Welcome " + name);
+        // ---------------------
+
+        //Rad sa brojevima
+//        int a,c,b;
+//        System.out.println("Enter 2 num");
+//        a = s.nextInt();
+//        b = s.nextInt();
+//        c = a+b;
+//        System.out.println("sum is " + c);
+        // ---------------------
+
+
+        // Ovo je da ispisemo celu recenicu
+//        Scanner sc=new Scanner(System.in);
+//        String x = sc.nextLine();
+//        System.out.println(x);
+        // ---------------------
+
+
+        // useRadix za pisanje binarninh brojeva koji se pretvaraju u normalne, postoji radix (2,8,10,16)
+//        Scanner sc=new Scanner(System.in);
+//        sc.useRadix(2);
+//        int x = sc.nextInt();
+//        System.out.println(x);
+        //---------------------
+
+
+
+        //RAD SA VARIJABLAMA
+
+//        byte b = 5;
+//        short s = 300;
+//        int i=100;
+//        float f=25.3f;
+//        char c='A';
+//        System.out.println(b);
+//        System.out.println(s);
+//        System.out.println(i);
+//        System.out.println(f);
+//        System.out.println(c);
+        //----------------------
+
+
+
+        // LITERALS
+
+//        byte b=15;
+//        System.out.println(b);
+//        float f = 2.5f;
+//        System.out.println(f);
+//        long l = 9999999999l;
+//        System.out.println(l);
+
+        // za lakse citanje long l=999_999_999; isto ce se prikazati kao 999999999
+        //-----------------------
+
+
+
+        //Pretvaranje normalnih brojeva u binarne
+//        int x = 5;
+//        System.out.println(Integer.toBinaryString(x));
+        //-----------------------
+
+
+
+
+    }
+}
