e756b741e115679b43249d43a7a8431712ef5b86
demir-subasic <subasicd2000@gmail.com>
2024-09-09 14:06:15 +0200

    loops

--- src/Main.java
+++ src/Main.java
@@ -282,6 +282,156 @@ public class Main {
 
 
 
+        // LOOPS
+
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter a Number");
+//        int n=sc.nextInt();
+//        long fact=1;
+//        for(int i=1;i<=n;i++)
+//        {
+//            fact=fact*i;
+//        }
+//        System.out.println("Factorial is "+fact);
+
+
+        // ovaj zadatak ce ceo broj podeliti u pojedinacne brojeve
+//        Scanner scan=new Scanner(System.in);
+//        System.out.println("Enter a Number");
+//        int n=scan.nextInt();
+//        int r;
+//        while(n>0)
+//        {
+//            r=n%10;
+//            n=n/10;
+//            System.out.println(r);
+//        }
+//        System.out.println(n);
+
+
+
+        //ovaj zadatak ce ddeliti ceo int broj dok ne dodje do nule
+//        Scanner scan=new Scanner(System.in);
+//        System.out.println("Enter a Number");
+//        int n=scan.nextInt();
+//        int count=0;
+//        while(n>0)
+//        {
+//            n=n/10;
+//            count++;
+//        }
+//        System.out.println(count);
+
+
+        // ovo je Arstrongov broj
+//        Scanner scan=new Scanner(System.in);
+//        System.out.println("Enter a Number");
+//        int n=scan.nextInt();
+//        int m=n;
+//        int sum=0;
+//        int r;
+//        while(n>0)
+//        {
+//            r=n%10;
+//            n=n/10;
+//            sum=sum+r*r*r;
+//        }
+//        if(sum==m)
+//            System.out.println("Its a Armstrong Number");
+//        else
+//            System.out.println("Its not an Armsttrong Number");
+
+
+
+
+        // ovo okrece broj i ispisuje ga suprotno - 257 = 752
+//        Scanner scan=new Scanner(System.in);
+//        System.out.println("Enter a number");
+//        int n = scan.nextInt();
+//        int rev=0,r;
+//        while(n>0){
+//            r=n%10;
+//            rev=rev*10+r;
+//            n/=10;
+//        }
+//        System.out.println("Reverse a number " + rev);
+
+
+
+        // Aritmeticka progresija- gde unosimo 3 broja(pocetni, koliko cemo uvecati i koliko puta ce se to desiti)
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Program to print AP Series");
+//        System.out.println("Enter a, d and n");
+//        int a=sc.nextInt();
+//        int d=sc.nextInt();
+//        int n=sc.nextInt();
+//        int term=a;
+//        for(int i=0;i<n;i++)
+//        {
+//            System.out.print(term+",");
+//            term=term+d;
+//        }
+
+
+
+        //Fibonacijev niz (gde je jedan broj zbir prethodna 2 broja)
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Program to Fibonacci Series");
+//        System.out.println("Enter number of Terms");
+//        int n=sc.nextInt();
+//        int a=0,b=1,c;
+//        System.out.print(a+","+b+",");
+//        for(int i=0;i<n-2;i++)
+//        {
+//            c=a+b;
+//            System.out.print(c+",");
+//            a=b;
+//            b=c;
+//        }
+
+
+
+        // Nested loops - matrice
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=1;j<=5;j++)
+//            {
+//                System.out.println(j+" ");
+//            }
+//            System.out.println("");
+//        }
+
+
+
+//        int count=0;
+//
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=1;j<=5;j++)
+//            {
+//                count++;
+//                System.out.format("%02d ",count);
+//            }
+//            System.out.println("");
+//        }
+
+
+
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=1;j<=5;j++)
+//            {
+//                if(i+j>5)
+//                    System.out.print("* ");
+//                else
+//                    System.out.print("  ");
+//            }
+//            System.out.println("");
+//        }
+
+
+
+
 
 
 
