75ca9216c82ff17a1f6f7d83cefb06ba0125d32f
demir-subasic <subasicd2000@gmail.com>
2024-09-13 20:51:48 +0200

    class and objects (get and set)

--- src/Main.java
+++ src/Main.java
@@ -672,6 +672,109 @@ public class Main {
 //    }
 //    fun(3) - ovo ce ispisati 3 2 1
 
+    // ----------------------------------------
+
+
+//    CLASS AND OBJECTS
+
+
+//    package java1;
+//    class Circle
+//    {
+//        public double radius;
+//
+//        public double area()
+//        {
+//            return Math.PI*radius*radius;
+//        }
+//        public double perimeter()
+//        {
+//            return 2*Math.PI*radius;
+//        }
+//        public double circumference()
+//        {
+//            return perimeter();
+//        }
+//    }
+//    public class Java1 {
+//
+//        public static void main(String[] args) {
+//            Circle c1=new Circle();
+//            c1.radius=7;
+//            System.out.println("Area:"+c1.area());
+//            System.out.println("Perimeter:"+c1.perimeter());
+//            System.out.println("Circumference:"+c1.circumference());
+//        }
+//
+//    }
+//
+//
+//
+//    // ovo je GETTER AND SETTER
+
+//    package rectangletest1;
+//    class Rectangle
+//    {
+//        private double length;
+//        private double breadth;
+//        public double getLength()
+//        {
+//            return length;
+//        }
+//        public double getBreadth()
+//        {
+//            return breadth;
+//        }
+//        public void setLength(double l)
+//        {
+//            if(l>=0)
+//                length=l;
+//            else
+//                length=0;
+//        }
+//        public void setBreadth(double b)
+//        {
+//            if(b>=0)
+//                breadth=b;
+//            else
+//                breadth=0;
+//        }
+//        public double area()
+//        {
+//            //return length*breadth;
+//            return getLength()*getBreadth();
+//        }
+//        public double perimeter()
+//        {
+//            return 2*(length+breadth);
+//        }
+//
+//        public boolean isSquare()
+//        {
+//            if(length==breadth)
+//                return true;
+//            else
+//                return false;
+//        }
+//    }
+//    public class RectangleTest1 {
+//        public static void main(String[] args) {
+//            Rectangle r=new Rectangle();
+//            r.setLength(10.5);//check with negative values.
+//            r.setBreadth(5.5);
+//            System.out.println("Area "+r.area());
+//            System.out.println("Perimeter "+r.perimeter());
+//            System.out.println("Is Square "+r.isSquare());
+//            System.out.println("Length "+r.getLength());
+//            System.out.println("Breadth "+r.getBreadth());
+//        }
+//    }
+
+    // --------------------------------------
+
+
+
+
 
 
 
